a4caea8ae0d66642780462c8ff42fc204e6af916
Wiktor Lewandowski <lewandowski.wiktor@yahoo.com>
2023-04-29 17:38:25 +0200

    keep fightin, keep pushin

--- src/index.js
+++ src/index.js
@@ -12,13 +12,33 @@ const filteredFields = '?fields=name,capital,population,flags,languages'
 const searchEl = document.getElementById("search-box");
 const listEl = document.querySelector('.country-list');
 const infoEl = document.querySelector('.country-info');
-let countryName = "";
 
 searchEl.addEventListener('input', debounce((event) => {
-    const inputEv = event.target.value;  
+    const inputEv = event.target.value.trim();  
     console.log(inputEv)    
-    return fetch(`${API_URL}${inputEv}${filteredFields}${searchParams}`)
-    .then((response) => response.json())
-    .then((data) => console.log(data))
+    return fetch(`${API_URL}${inputEv}${filteredFields}&${searchParams}`)
+    .then((response) => {
+        if (!response.ok && inputEv) {
+            Notiflix.Notify.failure("Oops, there is no country with that name");
+            throw new Error(response.status);
+        }
+        return response.json()
+    })
+    .then((data) => {
+        if(data.length > 10) {
+            Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+            return;
+        }
+        console.log(data, data.length, data[0].name, data[0].flags.svg)
+        return countryList(data) 
+    })
+    .catch((error) => console.log(error))
     }, DEBOUNCE_DELAY))
 
+function countryList(data) {
+     data.forEach(e => {
+        let li = document.createElement('li');
+        listEl.append(li)
+     });
+
+}
