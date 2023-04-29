f9969460e9fb1d6ddd0af1aebaee90e646e8f01f
Wiktor Lewandowski <lewandowski.wiktor@yahoo.com>
2023-04-29 14:11:02 +0200

    getting started

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
 import './css/styles.css';
-import debounce from 'lodash.debounce'
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 const API_URL = `https://restcountries.com/v3.1/name/`
+const searchParams = new URLSearchParams({
+    _limit: 10,
+    _sort: "name",
+});
+const filteredFields = '?fields=name,capital,population,flags,languages'
 const searchEl = document.getElementById("search-box");
 const listEl = document.querySelector('.country-list');
 const infoEl = document.querySelector('.country-info');
 let countryName = "";
 
-searchEl.addEventListener('input', (ev) => {            
-    return fetch(`${API_URL}${ev.currentTarget.value}`)
+searchEl.addEventListener('input', debounce((event) => {
+    const inputEv = event.target.value;  
+    console.log(inputEv)    
+    return fetch(`${API_URL}${inputEv}${filteredFields}${searchParams}`)
     .then((response) => response.json())
     .then((data) => console.log(data))
-    })
-
-function fetchCountries(name) {
-    return fetch(`${API_URL}${name}`)
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-}
-console.log(countryName)
-fetchCountries();
-
-// function processCountries()
+    }, DEBOUNCE_DELAY))
 
