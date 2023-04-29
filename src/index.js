9b12f5f37ffaea08ef0333952c1bd07e5e45ebe5
Wiktor Lewandowski <lewandowski.wiktor@yahoo.com>
2023-04-29 13:00:21 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce'
 
 const DEBOUNCE_DELAY = 300;
+const API_URL = `https://restcountries.com/v3.1/name/`
+const searchEl = document.getElementById("search-box");
+const listEl = document.querySelector('.country-list');
+const infoEl = document.querySelector('.country-info');
+let countryName = "";
+
+searchEl.addEventListener('input', (ev) => {            
+    return fetch(`${API_URL}${ev.currentTarget.value}`)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    })
+
+function fetchCountries(name) {
+    return fetch(`${API_URL}${name}`)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+}
+console.log(countryName)
+fetchCountries();
+
+// function processCountries()
+
